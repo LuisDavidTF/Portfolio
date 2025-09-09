1d8d58bac376b9e0e4019b2fde61071d15ed7a40
LuisDavidTF <luisdavidtf55@gmail.com>
2025-09-09 17:19:40 -0600

    Fix build: Node version and Hero import

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Skills from "./components/Skills";
-import Projects from "./components/projects";
+import Projects from "./components/Projects";
 import Footer from "./components/Footer";
 import "./App.css";
 
