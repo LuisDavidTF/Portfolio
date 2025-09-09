7cc1dbdc718f0c06b022332c9545221981275869
LuisDavidTF <luisdavidtf55@gmail.com>
2025-09-09 17:50:41 -0600

    Wrap App with HashRouter for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,17 @@
-// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "bootstrap/dist/css/bootstrap.min.css"; // Bootstrap CSS
-import "bootstrap/dist/js/bootstrap.bundle.min.js"; // Bootstrap JS
-import "@fortawesome/fontawesome-free/css/all.min.css"; // FontAwesome
+import { HashRouter as Router } from "react-router-dom"; // <- HashRouter
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import "./App.css"; // Tu CSS personalizado
+import "./App.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
